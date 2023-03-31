bcb9f84d8d0164be9d565b944e985eda45a8098b
kaeb13 <kaskebba@gmail.com>
2023-03-31 10:06:56 +0000

    Update JS file

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,175 +1,89 @@
 /*jshint esversion: 6 */
 
-// Create questions - and answers
-var q_a = document.querySelector(".q-a");
-var question = document.createElement("h1");
-var score = 0;
-var btn = document.createElement("button");
-btn.innerHTML = "Check Your Answer";
+const q_a = document.querySelector(".q-a");
+const question = document.createElement("h1");
+let score = 0;
+const btn = document.createElement("button");
+
+const questions = [
+    {
+        text: "What '80s TV series featured a talking car named KITT?",
+        answer: "knight rider"
+    },
+    {
+        text: "What TV series follows the lives of the Ewing family, a wealthy Texas oil family?",
+        answer: "dallas"
+    },
+    {
+        text: "Which TV series starred a group of Vietnam War veterans who worked as mercenaries?",
+        answer: "the a-team"
+    },
+    {
+        text: "What TV show featured a group of women working together in a New York City police precinct?",
+        answer: "cagney & lacey"
+    },
+    {
+        text: "What '80s TV series starred David Hasselhoff as a lifeguard?",
+        answer: "baywatch"
+    }
+];
+
+let currentQuestion = 0;
 
 function game() {
     score = 0;
-    var answer = document.createElement("input");
-    answer.setAttribute("answer", "4");
-    answer.id = "answer-input";
-    answer.setAttribute("autocomplete", "off");
-    answer.placeholder = "Type your answer...";
-    answer.setAttribute("second-answer", "four");
-    question.innerHTML = "How many members are in the music group ABBA?";
-    q_a.appendChild(question);
-    q_a.appendChild(answer);
-    q_a.appendChild(document.createElement("br"));
-    q_a.appendChild(btn);
-    btn.innerHTML = "submit";
-    btn.setAttribute("style", 
-      `font-size: 24px; background-color: #ff4c4c; 
-       cursor: pointer;
-       color: #ffff;
-       padding: 5px;
-       border-radius: 3px;
-       border: 1px solid white;
-       text-transform: uppercase;`);
+    currentQuestion = 0;
+    displayQuestion();
+}
 
-    // answer check and continue // 
-    btn.onclick = function() { checkAns1(); };
+function displayQuestion() {
+    if (currentQuestion < questions.length) {
+        question.innerHTML = questions[currentQuestion].text;
+        q_a.innerHTML = '';
+        q_a.appendChild(question);
 
-    function checkAns1() {
-      var answerInput = document.getElementById("answer-input");
-      var answerVal = answerInput.value.toLowerCase();
-      var mainAnswer = answerInput.getAttribute("answer");
-      var secondAnswer = answerInput.getAttribute("second-answer");
-      if(answerVal==mainAnswer || answerVal==secondAnswer) {
+        const answer = document.createElement("input");
+        answer.id = "answer-input";
+        answer.setAttribute("autocomplete", "off");
+        answer.placeholder = "Type your answer...";
+        q_a.appendChild(answer);
+
+        btn.innerHTML = "Submit";
+        btn.setAttribute("style",
+            `font-size: 24px; background-color: #ff4c4c;
+            cursor: pointer;
+            color: #ffff;
+            padding: 5px;
+            border-radius: 3px;
+            border: 1px solid white;
+            text-transform: uppercase;`);
+        q_a.appendChild(btn);
+
+        btn.onclick = function () { checkAnswer(); };
+    } else {
+        displayScore();
+    }
+}
+
+function checkAnswer() {
+    const answerInput = document.getElementById("answer-input");
+    const answerVal = answerInput.value.toLowerCase();
+
+    if (answerVal === questions[currentQuestion].answer) {
         alert("Correct answer!");
-        answerInput.value = "";
         score++;
-        question.innerHTML = "What is the capital of Sweden?";
-        answer.setAttribute("answer", "stockholm");
-        answer.setAttribute("second-answer", "Stockholm");
-        btn.onclick = function() { checkAns2(); } ;
-      } else {
+    } else {
         alert("Incorrect answer.");
-        answerInput.value = "";
-        question.innerHTML = "What is the capital of Sweden?";
-        answer.setAttribute("answer", "stockholm");
-        answer.setAttribute("second-answer", "Stockholm");
-        btn.onclick = function() { checkAns2(); } ;
-      }
     }
-    
-    function checkAns2() {
-  var answerInput = document.getElementById("answer-input");
-  var answerVal = answerInput.value.toLowerCase();
-  var mainAnswer = answerInput.getAttribute("answer");
-  var secondAnswer = answerInput.getAttribute("second-answer");
-  if(answerVal==mainAnswer || answerVal==secondAnswer) {
-    alert("Correct answer!");
-    answerInput.value = "";
-    score++;
-    question.innerHTML = "In which month do you celebrate swedish midsummer?";
-    answer.setAttribute("answer", "june");
-    answer.setAttribute("second-answer", "June");
-    btn.onclick = function() { checkAns3(); } ;
-  } else {
-    alert("Incorrect answer.");
-    answerInput.value = "";
-    question.innerHTML = "In which month do you celebrate swedish midsummer?";
-    answer.setAttribute("answer", "june");
-    answer.setAttribute("second-answer", "June");
-    btn.onclick = function() { checkAns3(); } ;
-  }
+
+    currentQuestion++;
+    displayQuestion();
 }
-  
 
-function checkAns3() {
-    var answerInput = document.getElementById("answer-input");
-    var answerVal = answerInput.value.toLowerCase();
-    var mainAnswer = answerInput.getAttribute("answer");
-    var secondAnswer = answerInput.getAttribute("second-answer");
-    if(answerVal==mainAnswer || answerVal==secondAnswer) {
-      alert("Correct answer!");
-      score++;
-      answerInput.value = "";
-      question.innerHTML =  "What is the Swedish world-famous furniture making company called?";
-      answer.setAttribute("answer", "ikea");
-      answer.setAttribute("second-answer", "");
-      btn.onclick = function() { checkAns4(); } ;
-    } else {
-      alert("Incorrect answer.");
-      answerInput.value = "";
-      question.innerHTML = "What is the Swedish world-famous furniture making company called?";
-      answer.setAttribute("answer", "ikea");
-      answer.setAttribute("second-answer", "");
-      btn.onclick = function() { checkAns4(); } ;
-    }
-  }
-  
-
-  function checkAns4() {
-    var answerInput = document.getElementById("answer-input");
-    var answerVal = answerInput.value.toLowerCase();
-    var mainAnswer = answerInput.getAttribute("answer");
-    var secondAnswer = answerInput.getAttribute("second-answer");
-    if(answerVal==mainAnswer) {
-      alert("Correct answer!");
-      answerInput.value = "";
-      score++;
-      question.innerHTML = " What is the colour of the cross on the Swedish flag?";
-      answer.setAttribute("answer", "yellow");
-      answer.setAttribute("second-answer", "");
-      btn.onclick = function() { checkAns5(); };
-    } else {
-  
-      alert("Incorrect answer.");
-      answerInput.value = "";
-      question.innerHTML = " What is the colour of the cross on the Swedish flag?";
-      answer.setAttribute("answer", "yellow");
-      answer.setAttribute("second-answer", "");
-      btn.onclick = function() { checkAns5(); } ;
-    }
-  }
-  function checkAns5() {
-  var answerInput = document.getElementById("answer-input");
-  var answerVal = answerInput.value.toLowerCase();
-  var mainAnswer = answerInput.getAttribute("answer");
-  var secondAnswer = answerInput.getAttribute("second-answer");
-  if(answerVal==mainAnswer) {
-    alert("Correct answer!");
-    score++;
+function displayScore() {
     question.innerHTML = "<span style='color: green;'>Your Score: " + score + "</span>";
-    answerInput.remove();
-    btn.innerHTML = "start over";
-    btn.style.background = "#ff2626";
-    answerInput.value = "";
-    btn.style.color = "white";
-    btn.style.textTransform = "uppercase";
-    btn.onclick = function() { game() ;} ;
-  } else {
-    alert("Incorrect answer.");
-    answerInput.remove();
-   
-    btn.innerHTML = "start again";
-    btn.style.background = "#E7625F";
-    btn.style.color = "white";
-    btn.style.textTransform = "uppercase";
-    btn.onclick = function() { game() ;};
-    question.innerHTML = "<span style='font-family: Lucida Console;'>Your Score: " + score + "</span>";
-  }
+    btn.innerHTML = "Start over";
+    btn.onclick = function () { game(); };
 }
 
-window.checkAns1 = checkAns1;
-window.checkAns2 = checkAns2;
-window.checkAns3 = checkAns3;
-window.checkAns4 = checkAns4;
-window.checkAns5 = checkAns5;
-}
-
-var start = document.createElement("button");
-start.innerText = "START";
-start.style.background = "#ff2626";
-start.style.color = "white";
-start.onclick = startGame();
-
-function startGame() {
- start.remove();
- game();
-}
+game();
